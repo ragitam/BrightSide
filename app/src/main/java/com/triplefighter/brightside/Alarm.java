98a4923847079e25bcd125a3a3869d3f6aeaab38
ragitam_6706144126 <agitarachmad.22@gmail.com>
2017-02-16 08:49:25 +0700

    first commit

--- app/src/main/java/com/triplefighter/brightside/Alarm.java
+++ app/src/main/java/com/triplefighter/brightside/Alarm.java
@@ -1,4 +1,4 @@
-package com.example.mavri.brightside;
+package com.triplefighter.brightside;
 
 
 import android.os.Bundle;
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 
